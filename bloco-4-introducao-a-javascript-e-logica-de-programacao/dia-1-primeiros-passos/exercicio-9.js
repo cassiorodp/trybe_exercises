ba0cbf307857333ca39c80de31285e176a95b5ec
Cássio Pereira <cassiorodp@gmail.com>
2021-05-19 18:56:45 -0300

    exercicio-9

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercicio-9.js
@@ -0,0 +1,13 @@
+let a = 8;
+let b = 2;
+let c = 6;
+let restoA = a % 2;
+let restoB = b % 2;
+let restoC = c % 2;
+
+if (restoA != 0 || restoB != 0 || restoC != 0) {
+  console.log('true');
+}
+else {
+  console.log('false');
+}
