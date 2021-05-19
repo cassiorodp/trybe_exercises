2c5eced143dbd94da98f06945364c17fe9e84db7
Cássio Pereira <cassiorodp@gmail.com>
2021-05-19 18:18:49 -0300

    exercicio-1

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercicio-1.js
@@ -0,0 +1,10 @@
+let a = 1;
+let b = 4;
+let sum = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+let modulo = a % b;
+
+console.log(sum);
+console.log(modulo);
