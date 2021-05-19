a96bd8ba7c58336d7785a8588396d761770ee7ca
Cássio Pereira <cassiorodp@gmail.com>
2021-05-19 16:44:39 -0300

    Bloco-4

---
+++ bloco_4/scripts.js
@@ -0,0 +1,51 @@
+// const name = "Cássio";
+// const birthCity = "Belém";
+// const birthYear = 2030;
+
+
+// console.log (name);
+// console.log (birthCity);
+// console.log (birthYear);
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+// console.log(typeof pacientAge);
+
+// let base = 5;
+// let altura = 8;
+// let area = base * altura;
+// let perimetro = 2 * base + 2 * altura;
+
+// console.log(base);
+// console.log(altura);
+// console.log(area);
+// console.log(perimetro);
+
+let candidateState = "lista"
+
+switch (candidateState) {
+  case "aprovada":
+    console.log("aprovada");
+    break;
+
+  case "lista":
+    console.log("lista");
+    break;
+
+  case "reprovada":
+    console.log("reprovada");
+    break;
+
+    default:
+      console.log ("Valor invalido");
+}
