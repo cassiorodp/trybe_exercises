b9d32abf2124477b8fb85bc19e3f835df42f8c2d
Cássio Pereira <cassiorodp@gmail.com>
2021-05-19 18:44:21 -0300

    exercicio-6

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/exercicio-6.js
@@ -0,0 +1,10 @@
+let xadrex = 'Rainha';
+
+switch (xadrex.toLowerCase()) {
+  case 'cavalo':
+    console.log('em L');
+    break;
+  case 'rainha':
+    console.log('todos os lados');
+    break;
+}
