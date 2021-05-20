001762754a80952f7370a754c5c4c434a96bf799
Cássio Pereira <cassiorodp@gmail.com>
2021-05-20 18:25:59 -0300

    exercicios

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-array-e-loop-for/exercicio-1.js
@@ -0,0 +1,92 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercicio 1
+// for (index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index]);
+// }
+
+//Exercicio 2
+// let soma = 0;
+
+// for (index = 0; index < numbers.length; index += 1) {
+// soma += numbers[index];
+// }
+// console.log(`Soma igual a ${soma}`);
+
+//Exercicio 3
+// let media = 0;
+// let soma = 0;
+
+// for (index = 0; index < numbers.length; index += 1) {
+// soma += numbers[index];
+// }
+// media = soma / numbers.length;
+// console.log(`A media e: ${media}`); 
+
+//Exercicio 4
+// let media = 0;
+// let soma = 0;
+
+// for (index = 0; index < numbers.length; index += 1) {
+// soma += numbers[index];
+// }
+// media = soma / numbers.length;
+// if (soma > 20) {
+//   console.log(`valor maior que 20`);
+// }
+// else {
+//   console.log(`valor menor ou igual a 20`);
+// }
+
+//Exercicio 5
+// let maiorNumero = numbers[2];
+
+// for (index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] > maiorNumero) {
+//     maiorNumero = numbers[index]
+//   }
+// }
+// console.log(`maior numero: ${maiorNumero}`);
+
+//Exercicio 6
+// let oddNumbers = 0; 
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if ((numbers[index] % 2) != 0) {
+//     oddNumbers += 1;
+//   }
+// }
+// if (oddNumbers > 0) {
+//   console.log(`numeros impares: ${oddNumbers}`);
+// }
+// else {
+//   console.log(`nenhum valor impar encontrado`);
+// }
+
+//Exercicio 7
+// let menorNumero = numbers[(numbers.length - 1)];
+
+// for (index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] < menorNumero) {
+//     menorNumero = numbers[index]
+//   }
+// }
+// console.log(`menor numero: ${menorNumero}`);
+
+//Exercicio 8
+// let contador = [];
+
+// for (index = 0; index <= 25; index +=1) {
+//   contador.push(index);
+// }
+// console.log(contador);
+
+//Exercicio 9
+// let contador = [];
+
+// for (let index = 0; index <= 25; index +=1) {
+//   contador.push(index);
+// }
+// for (let indexDivisao = 0; indexDivisao < contador.length; indexDivisao += 1 ) {
+//   console.log((contador[indexDivisao] / 2));
+// }
