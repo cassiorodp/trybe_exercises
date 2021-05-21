117f35deecc5c9b137c1ceeb3cd27c0c387f4877
Cássio Pereira <cassiorodp@gmail.com>
2021-05-21 15:19:31 -0300

    exercicio-1

---
+++ bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio-1.js
@@ -0,0 +1,10 @@
+let n = 5;
+let symbol = '*';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  symbol = symbol + '*'
+}
+for (let index = 0; index < n; index += 1) {
+  console.log(symbol)
+  
+}
