74ac5aea357936420c5bc9086bb75c9e6c3b46a9
Cássio Pereira <cassiorodp@gmail.com>
2021-05-28 19:40:24 -0300

    Exercicio 6

--- bloco-5-dom-eventos-e-web-storage/script.js
+++ bloco-5-dom-eventos-e-web-storage/script.js
@@ -88,3 +88,26 @@ buttonFriday.addEventListener('click', function () {
     }
   }
 })
+
+// Exercicio 6
+
+function dayMouseOver() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600';
+  })
+};
+
+function dayMouseOut() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseout', function(event) {
+    event.target.style.fontWeight = '200';
+    event.target.style.fontSize = '20px';
+  })
+};
+
+dayMouseOver();
+dayMouseOut();
