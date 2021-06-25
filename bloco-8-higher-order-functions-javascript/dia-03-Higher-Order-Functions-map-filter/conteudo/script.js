6e6fb604b09f9fd08bbeb790752483fedc2e87d0
Cássio Pereira <cassiorodp@gmail.com>
2021-06-25 15:19:00 -0300

    Dia 3

---
+++ bloco-8-higher-order-functions-javascript/dia-03-Higher-Order-Functions-map-filter/conteudo/script.js
@@ -0,0 +1,26 @@
+// const persons = [
+//   { firstName: 'Maria', lastName: 'Ferreira' },
+//   { firstName: 'João', lastName: 'Silva' },
+//   { firstName: 'Antonio', lastName: 'Cabral' },
+// ];
+
+// const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+// console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
