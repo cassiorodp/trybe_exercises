92a0b4f6cc990b66c4eb31603d6ef8ffbd9652e7
Cássio Pereira <cassiorodp@gmail.com>
2021-06-29 19:14:43 -0300

    Dia 5

---
+++ bloco-8-higher-order-functions-javascript/dia-05-spread-rest-e-destructuring/conteudo/script.js
@@ -0,0 +1,40 @@
+const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
+const patientInfo = [60, 1.7];
+
+// console.log(imc(...patientInfo)); // 20.76
+// console.log(...patientInfo);
+
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Uva', 'Melancia', 'Laranja'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Leite Condensado', 'Creme de leite', 'Leite em pó'];
+
+const fruitSalad = (fruit, additional) => [...fruit, ...additional];
+
+// console.log(fruitSalad(specialFruit, additionalItens));
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+// console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+// console.log(getPosition(-19.8157, -43.9542));
+
+const multiply = (number, value = 1) => {
+  // Escreva aqui a sua função
+  return number * value
+};
+
+console.log(multiply(8));
