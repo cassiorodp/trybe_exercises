1cda536fd4738b94aaf1828a241e78d909c618f1
Cássio Pereira <cassiorodp@gmail.com>
2021-07-15 19:31:24 -0300

    dia-03

---
+++ bloco-10-blocos-automatizados-com-jest/dia-03/conteudo/content-5.test.js
@@ -0,0 +1,66 @@
+const math = require('./content-5');
+
+// test('#subtrair', () => {
+//   math.subtrair = jest.fn();
+
+//   math.subtrair();
+//   expect(math.subtrair).toHaveBeenCalled();
+// });
+
+test('#multiplicar', () => {
+  math.multiplicar = jest.fn().mockReturnValue(10);
+
+  math.multiplicar();
+  expect(math.multiplicar).toHaveBeenCalled();
+  expect(math.multiplicar()).toBe(10);
+})
+
+test('#somar', () => {
+  math.somar = jest.fn().mockImplementation((a, b) => a + b);
+
+  math.somar(1, 2);
+  expect(math.somar).toHaveBeenCalled();
+  expect(math.somar).toHaveBeenCalledWith(1, 2);
+  expect(math.somar(1, 2)).toBe(3)
+})
+
+test('Testa chamada, retorno e parâmetros da função dividir', () => {
+  const mockDividir = jest.spyOn(math, "dividir");
+  mockDividir.mockReturnValue(15);
+  mockDividir.mockReturnValueOnce(2);
+  mockDividir.mockReturnValueOnce(5);
+
+  expect(mockDividir(4, 2)).toBe(2);
+  expect(mockDividir).toHaveBeenCalled();
+  expect(mockDividir).toHaveBeenCalledTimes(1);
+  expect(mockDividir).toHaveBeenCalledWith(4, 2);
+
+  expect(mockDividir(10, 2)).toBe(5);
+  expect(mockDividir).toHaveBeenCalled();
+  expect(mockDividir).toHaveBeenCalledTimes(2);
+  expect(mockDividir).toHaveBeenCalledWith(10, 2);
+
+  expect(mockDividir(30, 2)).toBe(15);
+  expect(mockDividir).toHaveBeenCalled();
+  expect(mockDividir).toHaveBeenCalledTimes(3);
+  expect(mockDividir).toHaveBeenCalledWith(30, 2);
+});
+
+test('Mock da função subtrair de modo que possa restaurar sua implementação original', () => {
+  const mockSubtrair = jest.spyOn(math, "subtrair");
+  mockSubtrair.mockImplementation((a, b) => a * b);
+  mockSubtrair.mockReturnValue(20);
+
+  expect(mockSubtrair(10, 2)).toBe(20);
+  expect(mockSubtrair).toHaveBeenCalled();
+  expect(mockSubtrair).toHaveBeenCalledTimes(1);
+  expect(mockSubtrair).toHaveBeenCalledWith(10, 2);
+
+  mockSubtrair.mockRestore();
+  mockSubtrair.mockReturnValue(8);
+
+  expect(mockSubtrair(10, 2)).toBe(8);
+  expect(mockSubtrair).toHaveBeenCalled();
+  expect(mockSubtrair).toHaveBeenCalledTimes(1);
+  expect(mockSubtrair).toHaveBeenCalledWith(10, 2);
+});
