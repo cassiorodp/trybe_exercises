cc99a7142a2da168985d5b8a3afd84cb46cbc1bb
Cássio Pereira <cassiorodp@gmail.com>
2021-07-20 15:59:49 -0300

    Bloco 11 dia 01 conteudo

---
+++ bloco-11-introducao-ao-react/dia-01-hello-world-no-react/conteudo/new-app/src/Component.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+class description extends Component {
+  render() {
+    return <div><h1>Cássio Pereira</h1>
+    <p>Uma descrição</p></div>
+  }
+}
+
+export default description
