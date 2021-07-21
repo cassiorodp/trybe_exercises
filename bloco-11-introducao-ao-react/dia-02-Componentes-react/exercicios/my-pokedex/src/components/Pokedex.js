222514c286cb88be10dc4c06a6a3de3b9f6e0b44
Cássio Pereira <cassiorodp@gmail.com>
2021-07-21 19:07:03 -0300

    Bloco 11 dia 02

---
+++ bloco-11-introducao-ao-react/dia-02-Componentes-react/exercicios/my-pokedex/src/components/Pokedex.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import pokemons from '../data';
+import Pokemon from './Pokemon';
+
+class Pokedex extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Pokedex</h1>
+        <div className = 'pokedex'>
+        {pokemons.map(pokemon => <Pokemon key = {pokemon.id}  pokemonsInfo = {pokemon}/>)}
+        </div>
+      </main>
+    )
+  }
+}
+
+export default Pokedex
