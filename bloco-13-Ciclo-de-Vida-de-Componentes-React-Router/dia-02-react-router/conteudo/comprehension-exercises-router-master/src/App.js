d495429a9908d840cee2970c0c8fbaf222de1d5f
Cássio Pereira <cassiorodp@gmail.com>
2021-07-30 15:10:33 -0300

    bloco 13 dia 02

--- bloco-13-Ciclo-de-Vida-de-Componentes-React-Router/dia-02-react-router/conteudo/comprehension-exercises-router-master/src/App.js
+++ bloco-13-Ciclo-de-Vida-de-Componentes-React-Router/dia-02-react-router/conteudo/comprehension-exercises-router-master/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
-        <Route path='/users' component={Users} />
+        <Route path='/users/:id' render={(props) => <Users {...props} greetingsMessage='Good Morning'/>} />
         <ul>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/about'>About</Link></li>
