2e1f76034f86f830edba99a1f0fbd5d440985839
Cássio Pereira <cassiorodp@gmail.com>
2021-08-24 19:11:31 -0300

    bloco 15 dia 02

---
+++ bloco-16-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/exercises/exercise-react-with-redux-intro/src/redux/index.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+import trafficReducer from './trafficReducer';
+import carReducer from './carReducer';
+
+const rootReducer = combineReducers({trafficReducer, carReducer})
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
