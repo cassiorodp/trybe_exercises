d83740e7035820146a61932e8ab357ffcd375ae2
Cássio Pereira <cassiorodp@gmail.com>
2021-09-14 18:47:39 -0300

    bloco 18-dia-02

---
+++ bloco-18-context-api-e-react-hooks/dia-02-react-hooks-useState-useContext/exercicios/exercise-react-hooks-refactoring/src/context/Provider.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import CarsContext from './CarsContext';
+
+function Provider({children}) {
+
+  const [cars, setCars] = useState({ redCar: false, blueCar: false, yellowCar: false });
+  const [signal, setSignal] = useState({ color: 'red' });
+
+  const moveCar = (car, side) => {
+    setCars({...cars, [car]: side})
+  };
+
+  const changeSignal = (signalColor) => {
+    setSignal({...signal, color: signalColor})
+  };
+
+  const contextValue = {
+    cars,
+    signal,
+    moveCar,
+    changeSignal,
+  }
+
+  return (
+    <CarsContext.Provider value={ contextValue }>
+      {children}
+    </CarsContext.Provider>
+  );
+};
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
