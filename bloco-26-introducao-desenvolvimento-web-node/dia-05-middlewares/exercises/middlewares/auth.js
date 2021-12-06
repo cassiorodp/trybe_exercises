b7877e1f48376bb8f67a0604ead0bcda8e8da72b
Cássio Pereira <cassiorodp@gmail.com>
2021-12-06 18:33:23 -0300

    bloco 26 dia 05

--- bloco-26-introducao-desenvolvimento-web-node/dia-05-middlewares/exercises/middlewares/auth.js
+++ bloco-26-introducao-desenvolvimento-web-node/dia-05-middlewares/exercises/middlewares/auth.js
@@ -2,10 +2,13 @@ module.exports = (req, res, next) => {
   const {email, password} = req.body;
   const rePassword = /^[0-9]\w{4,8}$/;
 
+  const token = '86567349784e';
+
   if (!(email.includes('@') && email.includes('.com'))) {
     return res.status(400).json({ message: 'invalid email' })
   }
 
   if (!rePassword.test(password)) return res.status(400).json({ message: 'invalid password'});
-  
+
+  return res.status(200).json({'token': token})/
 }
