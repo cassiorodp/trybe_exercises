2f465c35369e869c6e000a79a5f3fb4b16bf9bdd
Cassio Pereira <cassiorodp@gmail.com>
2022-01-14 20:33:18 -0300

    bloco 28 dia-03

--- bloco-28-autenticacao-e-upload-de-arquivos/dia-03-testando-apis-com-testes-de-integração/exercises/nodejs-jwt-base-project/tests/findUserById.test.js
+++ bloco-28-autenticacao-e-upload-de-arquivos/dia-03-testando-apis-com-testes-de-integração/exercises/nodejs-jwt-base-project/tests/findUserById.test.js
@@ -15,96 +15,96 @@ const EXAMPLE_ID = '605de6ded1ff223100cd6aa1'
 
 
 describe('GET /api/users/:userId', () => {
-  // describe('Quando não é passado um JWT para autenticação', () => {
-  //   let response;
-  //   before(async () => {
-  //     response = await chai.request(server)
-  //       .get(`/api/users/${EXAMPLE_ID}`)
-  //       .set('authorization', '');
-  //   });
+  describe('Quando não é passado um JWT para autenticação', () => {
+    let response;
+    before(async () => {
+      response = await chai.request(server)
+        .get(`/api/users/${EXAMPLE_ID}`)
+        .set('authorization', '');
+    });
 
-  //   after(() => {
-  //     //
-  //   });
+    after(() => {
+      //
+    });
 
-  //   it('retorna código de status "400"', () => {
-  //     expect(response).to.have.status(401);
-  //   });
+    it('retorna código de status "400"', () => {
+      expect(response).to.have.status(401);
+    });
 
-  //   it('retorna um objeto no body', () => {
-  //     expect(response.body).to.be.an('object');
-  //   });
+    it('retorna um objeto no body', () => {
+      expect(response.body).to.be.an('object');
+    });
 
-  //   it('objeto de resposta possui a propriedade "error"', () => {
-  //     expect(response.body).to.have.property('error');
-  //   });
+    it('objeto de resposta possui a propriedade "error"', () => {
+      expect(response.body).to.have.property('error');
+    });
 
-  //   it('a propriedade "error" possui a mensagem "Token não encontrado ou informado"', () => {
-  //     expect(response.body.error).to.be.equal('Token não encontrado ou informado');
-  //   });
-  // });
+    it('a propriedade "error" possui a mensagem "Token não encontrado ou informado"', () => {
+      expect(response.body.error).to.be.equal('Token não encontrado ou informado');
+    });
+  });
 
-  // describe('Quando o usuário solicita informações de outro usuário', () => {
-  //   let response;
-  //   const DBServer = new MongoMemoryServer();
+  describe('Quando o usuário solicita informações de outro usuário', () => {
+    let response;
+    const DBServer = new MongoMemoryServer();
 
-  //   before(async () => {
-  //     const connectionMock = await DBServer.getUri()
-  //       .then(URLMock => MongoClient.connect(
-  //         URLMock,
-  //         { useNewUrlParser: true, useUnifiedTopology: true }
-  //       ));
+    before(async () => {
+      const connectionMock = await DBServer.getUri()
+        .then(URLMock => MongoClient.connect(
+          URLMock,
+          { useNewUrlParser: true, useUnifiedTopology: true }
+        ));
 
-  //     sinon.stub(MongoClient, 'connect')
-  //       .resolves(connectionMock);
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
 
-  //     connectionMock.db('jwt_exercise')
-  //       .collection('users')
-  //       .insertOne({
-  //         _id: EXAMPLE_ID,
-  //         username: 'fake-user',
-  //         password: 'fake-password',
-  //         name: 'fake-name',
-  //         birthdate: '01/01/1960',
-  //         biography: 'fake-biography',
-  //       })
+      connectionMock.db('jwt_exercise')
+        .collection('users')
+        .insertOne({
+          _id: EXAMPLE_ID,
+          username: 'fake-user',
+          password: 'fake-password',
+          name: 'fake-name',
+          birthdate: '01/01/1960',
+          biography: 'fake-biography',
+        })
 
-  //     const token = await chai.request(server)
-  //       .post('/api/login')
-  //       .send({
-  //         username: 'fake-user',
-  //         password: 'fake-password'
-  //       })
-  //       .then((res) => res.body.token);
+      const token = await chai.request(server)
+        .post('/api/login')
+        .send({
+          username: 'fake-user',
+          password: 'fake-password'
+        })
+        .then((res) => res.body.token);
 
-  //     const OTHER_EXAMPLE_ID = '565de6ded1ff223100cd6aa2'
+      const OTHER_EXAMPLE_ID = '565de6ded1ff223100cd6aa2'
 
-  //     response = await chai.request(server)
-  //       .get(`/api/users/${OTHER_EXAMPLE_ID}`)
-  //       .set('authorization', token);
-  //   });
+      response = await chai.request(server)
+        .get(`/api/users/${OTHER_EXAMPLE_ID}`)
+        .set('authorization', token);
+    });
 
-  //   after(async () => {
-  //     MongoClient.connect.restore();
-  //     await DBServer.stop();
-  //   });
+    after(async () => {
+      MongoClient.connect.restore();
+      await DBServer.stop();
+    });
 
-  //   it('retorna código de status "401"', () => {
-  //     expect(response).to.have.status(401);
-  //   });
+    it('retorna código de status "401"', () => {
+      expect(response).to.have.status(401);
+    });
 
-  //   it('retorna um objeto no body', () => {
-  //     expect(response.body).to.be.an('object');
-  //   });
+    it('retorna um objeto no body', () => {
+      expect(response.body).to.be.an('object');
+    });
 
-  //   it('objeto de resposta possui a propriedade "error"', () => {
-  //     expect(response.body).to.have.property('error');
-  //   });
+    it('objeto de resposta possui a propriedade "error"', () => {
+      expect(response.body).to.have.property('error');
+    });
 
-  //   it('a propriedade "error" possui a mensagem "Acesso negado"', () => {
-  //     expect(response.body.error).to.be.equal('Acesso negado');
-  //   });
-  // });
+    it('a propriedade "error" possui a mensagem "Acesso negado"', () => {
+      expect(response.body.error).to.be.equal('Acesso negado');
+    });
+  });
 
   describe('Quando a pessoa usuária é encontrada com sucesso', () => {
     let response;
@@ -139,9 +139,7 @@ describe('GET /api/users/:userId', () => {
         
         response = await chai.request(server)
         .get(`/api/users/${EXAMPLE_ID}`)
-        .set('authorization', token);
-        console.log(response,'response');
-        
+        .set('authorization', token);        
     });
 
     after(async () => {
